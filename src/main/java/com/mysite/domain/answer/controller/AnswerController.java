46d28f41128637119bbe53cddf9ef4a23866d833
Anwooseong <aws5624@naver.com>
2023-11-22 01:01:59 +0900

    Feat: 답변 등록

---
+++ src/main/java/com/mysite/domain/answer/controller/AnswerController.java
@@ -0,0 +1,29 @@
+package com.mysite.domain.answer.controller;
+
+import ch.qos.logback.core.model.Model;
+import com.mysite.domain.answer.service.AnswerService;
+import com.mysite.domain.question.entity.Question;
+import com.mysite.domain.question.service.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+@Controller
+public class AnswerController {
+
+    private final QuestionService questionService;
+    private final AnswerService answerService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content) {
+        Question question = this.questionService.getQuestion(id);
+        // TODO: 답변을 저장한다.
+        answerService.create(question, content);
+        return String.format("redirect:/question/detail/%s", id);
+    }
+}

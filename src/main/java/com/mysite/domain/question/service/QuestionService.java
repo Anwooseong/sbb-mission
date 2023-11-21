1b58c1150ff2d066f31d9bcc30858e3fdf68a663
Anwooseong <aws5624@naver.com>
2023-11-22 00:57:03 +0900

    Feat: 질문 상세 조회

--- src/main/java/com/mysite/domain/question/service/QuestionService.java
+++ src/main/java/com/mysite/domain/question/service/QuestionService.java
@@ -2,10 +2,12 @@ package com.mysite.domain.question.service;
 
 import com.mysite.domain.question.entity.Question;
 import com.mysite.domain.question.repository.QuestionRepository;
+import com.mysite.global.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,5 +17,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
